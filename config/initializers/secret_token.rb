0de52c9074530cb102bba716f0cbe852bb59abd5
xiasiyu@gmail.com <xiasiyu@gmail.com>
2012-11-28 18:51:31 +0800

    initial project for TWBT-2012-11-28

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TWBT::Application.config.secret_token = '06214ce43a0d1e91639db4fa2ec03c6181bdd0053117ce70b7a03860258cb6b31cdc1c559624109a2db8c12f9de7455a07a8e8a2829e44aa1cdbbba1ecdd047b'

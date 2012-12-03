4e172daa7cfb487d97d927ba3be77cbd7e4b48fa
xiasiyu@gmail.com <xiasiyu@gmail.com>
2012-12-03 16:55:42 +0800

    fix build.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TWBT::Application.config.session_store :cookie_store, key: '_TWBT_session'
+#TWBT::Application.config.session_store :cookie_store, key: '_TWBT_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

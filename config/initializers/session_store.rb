04d0f59c248de7c1fca6be783da98f6389dbb9d8
xiasiyu@gmail.com <xiasiyu@gmail.com>
2012-12-03 17:04:03 +0800

    .

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 TWBT::Application.config.session_store :cookie_store, key: '_TWBT_session'
-end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
